8a2b8da31df9b5809fb050cee7aeebe91e2c12b6
delvinlow <delvinlowzy@gmail.com>
2020-08-21 00:41:01 +0800

    Add more test cases for different graphs

--- CriticalConnections/tests/test_critical_connections.py
+++ CriticalConnections/tests/test_critical_connections.py
@@ -8,13 +8,32 @@ class TestCriticalConnections(unittest.TestCase):
         critical_edges = Solution().criticalConnections(4, input_edge_list)
         self.assertEqual(critical_edges, [[1, 3]])
 
+    def test_single_critical_edge_two_nodes_returns_one(self):
+        input_edge_list = [[0, 1]]
+        critical_edges = Solution().criticalConnections(1, input_edge_list)
+        self.assertEqual(critical_edges, [[0, 1]])
+
     def test_double_critical_edges_returns_both(self):
         input_edge_list = [[1, 0], [2, 1], [0, 2], [3, 1], [4, 1]]
         critical_edges = Solution().criticalConnections(5, input_edge_list)
         self.assertEqual(critical_edges, [[1, 3], [1, 4]])
+
+    def test_fully_connected_triangle_graph_returns_none(self):
+        input_edge_list = [[0, 1], [1, 2], [2, 0]]
+        critical_edges = Solution().criticalConnections(3, input_edge_list)
+        self.assertEqual(critical_edges, [])
 
     def test_star_graph_returns_all_edges(self):
         input_edge_list = [[1, 0], [2, 1], [3, 1], [4, 1]]
         critical_edges = Solution().criticalConnections(4, input_edge_list)
         self.assertEqual(critical_edges, [[0, 1], [1, 2], [1, 3], [1, 4]])
 
+    def test_two_cycle_connected_with_one_edge_returns_the_critical_edge(self):
+        first_cycle = [[0, 1], [1, 2], [2, 5], [0, 3], [3, 4], [4, 5]]
+        second_cycle = [[6, 9], [6, 7], [7, 8], [8, 11], [10, 11], [9, 10]]
+        critical_edge = [[5, 6]]
+        input_edge_list = first_cycle + second_cycle + critical_edge
+
+        critical_edges = Solution().criticalConnections(13, input_edge_list)
+
+        self.assertEqual(critical_edges, [[5, 6]])
--- CriticalConnections/tests/test_edge_list.py
+++ CriticalConnections/tests/test_edge_list.py
@@ -39,3 +39,15 @@ class TestEdgeListConversion(unittest.TestCase):
         expected_adjacency_list = {0: [1], 1: [0, 2, 3, 4], 2: [1], 3: [1], 4: [1]}
         output_adjacency_list = EdgeList(input_edge_list).to_adjacency_list()
         self.assertEqual(output_adjacency_list, expected_adjacency_list)
+
+    def test_two_cycle_connected_with_one_edge_to_adjacency_list(self):
+        first_cycle = [[0, 1], [1, 2], [2, 5], [0, 3], [3, 4], [4, 5]]
+        second_cycle = [[6, 9], [6, 7], [7, 8], [8, 11], [10, 11], [9, 10]]
+        critical_edge = [[5, 6]]
+        input_edge_list = first_cycle + second_cycle + critical_edge
+
+        expected_adjacency_list = {0: [1, 3], 1: [0, 2], 2: [1, 5], 3: [0, 4],
+            4: [3, 5], 5: [2, 4, 6], 6: [9, 7, 5], 7: [6, 8], 8: [7, 11], 9: [6, 10], 10: [11, 9], 11: [8, 10]}
+        output_adjacency_list = EdgeList(input_edge_list).to_adjacency_list()
+
+        self.assertEqual(output_adjacency_list, expected_adjacency_list)
